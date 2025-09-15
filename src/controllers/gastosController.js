21552a6c173d1b5d5715024f7d9b54dc4af99f9f
Kerlen Melo <kerlen_1@hotmail.com>
2025-09-15 20:59:57 -0300

    Feature 2: Implementando a rota POST para adicionar gastos

--- src/controllers/gastosController.js
+++ src/controllers/gastosController.js
@@ -5,4 +5,16 @@ const getGastos = (req, res) => {
   res.json(gastos); // Retorna todos os gastos
 };
 
-module.exports = { getGastos };
+const addGastos = (req, res) => {
+  const { titulo, categoria } = req.body;
+  if (!titulo || !categoria) {
+    return res.status(400).json({ message: 'Título e Categoria são obrigatórios.' });
+  }
+
+  const newGasto = { id: gastos.length + 1, titulo, categoria };
+  gastos.push(newGasto);
+  res.status(201).json(newGasto);
+};
+
+
+module.exports = { getGastos, addGastos };
--- src/routes/gastosRoutes.js
+++ src/routes/gastosRoutes.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const gastosController = require('../controllers/gastosController');
 
-router.get('/', gastosController.getGastos); // Rota para listar os gastos
+router.get('/', gastosController.getGastos);
+
+router.post('/', gastosController.addGastos)
 
 module.exports = router;
