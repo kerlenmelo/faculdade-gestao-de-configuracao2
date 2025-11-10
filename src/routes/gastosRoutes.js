18563228ee54057f1fc96abfcc2e3eb33bb2766e
Kerlen Melo <kerlen_1@hotmail.com>
2025-11-10 11:55:38 -0300

    feat: Adiciona rota DELETE /api/gastos/:id

--- src/routes/gastosRoutes.js
+++ src/routes/gastosRoutes.js
@@ -3,8 +3,20 @@ const express = require('express');
 const router = express.Router();
 const gastosController = require('../controllers/gastosController');
 
-router.get('/', gastosController.getGastos);
+router.get('/', gastosController.getGastos)
 
 router.post('/', gastosController.addGastos)
+
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const index = gastos.findIndex(gasto => gasto.id == id)
+
+    if (index === -1){
+        return res.status(404).json({ message: 'Gasto não encontrado' })
+    }
+
+    gastos.splice(index, 1)
+    res.status(204).send()
+})
 
 module.exports = router;
